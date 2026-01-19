0c3f856f83baf035ebcfd46545cee263432249fe
Ilia Ablamonov <ilia@flamefork.ru>
2026-01-19 19:25:27 +0100

    Minor fixes (#6)
    * Modernize pytest options
    * Fix query discovery
    * Fix input object defaults

--- src/iron_gql/generator.py
+++ src/iron_gql/generator.py
@@ -177,7 +177,7 @@ def find_fn_calls(
     root_path: Path, fn_name: str, *, skip_path: Path
 ) -> Iterator[tuple[Path, int, ast.Call]]:
     for path in root_path.glob("**/*.py"):
-        if path.is_relative_to(skip_path):
+        if path.resolve() == skip_path.resolve():
             continue
         content = path.read_text(encoding="utf-8")
         if fn_name not in content:
@@ -278,6 +278,7 @@ class GQLModel(pydantic.BaseModel):
         populate_by_name=True,
         alias_generator={to_camel_fn_full_name.replace(":", ".")},
         extra="forbid",
+        validate_default=True,
     )"""
 
 
